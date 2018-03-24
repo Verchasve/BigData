23bd2d60d4bcd9f5a5c78a917513ea0dba0ae8c1
Verch_HP <Verch_HP@10.14.212.207>
2018-03-24 10:36:35 -0400

    Next Class

---
+++ BigDataDominance/src/searchQuery/QueryClass.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package searchQuery;
+
+/**
+ * @author Verch_HP
+ *
+ */
+public class QueryClass {
+
+	/**
+	 * 
+	 */
+	public QueryClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
